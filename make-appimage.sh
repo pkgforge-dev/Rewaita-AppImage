c06310e38fcd80abc4f2b2be101280864d2c4db7
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-04-13 10:32:01 +0200

    Replace self-updater.bg.hook with self-updater.hook

--- make-appimage.sh
+++ make-appimage.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 VERSION=$(pacman -Q rewaita | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/scalable/apps/io.github.swordpuffin.rewaita.svg
 export DESKTOP=/usr/share/applications/io.github.swordpuffin.rewaita.desktop
