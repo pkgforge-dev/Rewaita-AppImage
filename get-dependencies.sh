ef84c8ad4077b4097be24164acea8bce2e12ee3d
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-05-01 01:44:20 +0200

    Add python-pyciede2000 to package dependencies installation

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -11,4 +11,5 @@ get-debloated-pkgs --add-common --prefer-nano
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 make-aur-package python-fortune-python
+make-aur-package python-pyciede2000
 make-aur-package rewaita
